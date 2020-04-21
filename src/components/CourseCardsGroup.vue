89a0c8ab6c2aa84eb368469c413b9da4c9d1dd8a
Cailice <3047673038@qq.com>
2020-04-21 10:27:15 +0800

    提交课程负责人的UI 1.0
    提交课程负责人的一些组件

---
+++ src/components/CourseCardsGroup.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <v-row>
+            <v-col v-for="(item, i) in items" :key="i" cols="6" md="3">
+                <v-card class="mx-auto" max-width="344" raised outlined 
+                    @click="clickCard">
+                    <v-list-item three-line>
+                        <v-list-item-content>
+                            <div class="overline mb-4">学时：{{ item.hour }}</div>
+                            
+                            <v-list-item-title class="headline mb-1">{{ item.name }}</v-list-item-title>
+                            
+                            <v-list-item-subtitle>学分: {{item.score}}</v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            items: [{
+                    name: "IT项目管理",
+                    hour: "54",
+                    score: "4.5",
+                },
+                {
+                    name: "软件工程",
+                    hour: "54",
+                    score: "4",
+                },
+                {
+                    name: "计算机网络",
+                    hour: "54",
+                    score: "3",
+                },
+                {
+                    name: "计算机组成原理",
+                    hour: "54",
+                    score: "4",
+                }
+            ],
+            selected: [],
+        }),
+        methods: {
+            clickCard: function () {
+                this.$router.push({path: '/courseLeaderRequestsList'})
+            },
+        }
+    }
+</script>
