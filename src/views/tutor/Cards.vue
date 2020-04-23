2b9baefca8e22e540b675f0b5947fda4dcc75b57
cjc7373 <niuchangcun@163.com>
2020-04-23 21:59:31 +0800

    收工 睡觉

--- src/views/tutor/Cards.vue
+++ src/views/tutor/Cards.vue
@@ -1,7 +1,7 @@
 <template>
     <div style="padding: 20px">
         <v-row>
-            <v-col v-for="(item, i) in items" :key="i" cols="6" md="3">
+            <v-col v-for="(item, i) in classes" :key="i" cols="6" md="3">
                 <v-card class="mx-auto" max-width="344" raised outlined 
                     @click="click_card">
                     <v-list-item three-line>
@@ -10,7 +10,7 @@
                             
                             <v-list-item-title class="headline mb-1">{{ item.name }}</v-list-item-title>
                             
-                            <v-list-item-subtitle>人数: {{item.peaple}}</v-list-item-subtitle>
+                            <v-list-item-subtitle>人数: {{item.student_number}}</v-list-item-subtitle>
                         </v-list-item-content>
                     </v-list-item>
                 </v-card>
@@ -22,34 +22,26 @@
 <script>
     export default {
         data: () => ({
-            items: [{
-                    year: "2017",
-                    name: "一班",
-                    peaple: "33",
-                },
-                {
-                    year: "2017",
-                    name: "二班",
-                    peaple: "32",
-                },
-                {
-                    year: "2017",
-                    name: "三班",
-                    peaple: "29",
-                },
-                {
-                    year: "2017",
-                    name: "四班",
-                    peaple: "34",
-                },
-            ],
             selected: [],
+            classes: []
         }),
         methods: {
             click_card: function() {
                 this.$router.push({ path: "students", query: { id: 1111 } })
             },
-        }
+        },
+        created() {
+            this.$axios
+                .get('user/tutor/?username=' + this.username)
+                .then(response => {
+                    this.classes = response.data.squad
+                })
+        },
+        computed: {
+            username: function() {
+                return this.$store.state.username
+            }
+        },
 
     }
 </script>
