4b5984f31e8a53644552d48b4e7e63705b9e094b
Cailice <3047673038@qq.com>
2020-04-24 21:13:24 +0800

    修补(课程负责人前端)：完善统计功能和内部数据结构
    还差审核的逻辑以及获取后端的数据后如何处理的模块

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,28 @@ export default new Vuex.Store({
   state: {
     role: undefined,
     name: undefined,
-    username: undefined
+    username: undefined,
+    evaluationValues: [
+      {
+        courseName: "软件工程",
+        teacherName: "王二小",
+        values: [
+          { indexNo: "1-1", list: [0.1, 0.2, 0.3, 0.4, 0.5] },
+          { indexNo: "1-2", list: [0.6, 0.2, 0.7, 0.4, 0.9] },
+          { indexNo: "1-3", list: [0.2, 0.5, 0.8, 0.3, 0.2] }
+        ]
+      },
+      {
+        courseName: "软件工程",
+        teacherName: "李小三",
+        values: [
+          { indexNo: "2-1", list: [0.2, 0.1, 0.4, 0.3, 0.1] },
+          { indexNo: "2-2", list: [0.7, 0.1, 0.8, 0.2, 0.4] },
+          { indexNo: "2-3", list: [0.3, 0.4, 0.9, 0.6, 0.6] }
+        ]
+      }
+    ],
+    currentEvalutionValue: undefined
   },
   mutations: {
     set_role(state, role) {
@@ -18,6 +39,12 @@ export default new Vuex.Store({
     },
     set_username(state, username) {
       state.username = username
+    },
+    set_evalution_values(state, evalutionValues) {
+      state.evalutionValues = evalutionValues
+    },
+    set_current_evalution_value(state, currentEvalutionValue) {
+      state.currentEvalutionValue = currentEvalutionValue
     }
   },
   actions: {
