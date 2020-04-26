b600c2f68119e69bee1a82b04de025f6dffb7f7a
cjc7373 <niuchangcun@163.com>
2020-04-26 14:02:13 +0800

    教师录入评价值

--- src/views/teacher/Cards.vue
+++ src/views/teacher/Cards.vue
@@ -4,51 +4,75 @@
         <v-divider></v-divider>
         <v-row>
             <v-col v-for="(item, i) in courses" :key="i" cols="6" md="3">
-                <v-card class="mx-auto" max-width="344" raised outlined 
-                    @click="click_card(item.id)">
+                <v-card
+                    class="mx-auto"
+                    max-width="344"
+                    raised
+                    outlined
+                    @click="click_card(item.id)"
+                >
                     <v-list-item three-line>
                         <v-list-item-content>
                             <div class="overline mb-4">课程id: {{item.id}}</div>
-                            
-                            <v-list-item-title class="headline mb-1">{{ item.name }}</v-list-item-title>
-                            
-                            <v-list-item-subtitle>人数: {{item.student_number}}</v-list-item-subtitle>
+
+                            <v-list-item-title class="headline mb-1">{{ item.offering_course.name }}</v-list-item-title>
+
+                            <v-list-item-subtitle>人数: {{item.students.length}}</v-list-item-subtitle>
+
+                            <v-list-item-subtitle>审核状态: {{item.review_status}}</v-list-item-subtitle>
+
+                            <v-list-item-subtitle
+                                v-if="item.review_status == '未通过'"
+                            >不通过原因: {{item.review_comment}}</v-list-item-subtitle>
                         </v-list-item-content>
                     </v-list-item>
                 </v-card>
+
+                <v-dialog v-model="dialog" max-width="520px">
+                    <v-btn color="primary" @click="add_grade">新增评价值</v-btn>
+                    <v-btn color="primary" @click="modify_grade">修改评价值</v-btn>
+                </v-dialog>
             </v-col>
         </v-row>
     </div>
 </template>
 
 <script>
-    export default {
-        data: () => ({
-            courses: [],
-            selected: [],
-        }),
-        created() {
-            this.fetch_data()
+export default {
+    data: () => ({
+        courses: [],
+        selected: [],
+        dialog: false,
+        edit_course_id: -1
+    }),
+    created() {
+        this.fetch_data();
+    },
+    methods: {
+        click_card: function(course_id) {
+            this.edit_course_id = course_id;
+            this.dialog = true;
         },
-        methods: {
-            click_card: function(course_id) {
-                this.$router.push({ path: "input", query: { course_id: course_id } })
-            },
-            fetch_data: function() {
-                this.$axios
-                    .get('user/teacher/?username=' + this.username)
-                    .then(response => {
-                        console.log(response)
-                        this.courses = response.data.courses
-                    })
-            }
+        fetch_data: function() {
+            this.$axios
+                .get("course/courses/?teacher_username=" + this.username)
+                .then(response => {
+                    console.log(response);
+                    this.courses = response.data.courses;
+                });
         },
-        computed: {
-            username: function() {
-                return this.$store.state.username
-            }
+        add_grade: function() {
+            this.$router.push({
+                path: "input",
+                query: { course_id: this.edit_course_id }
+            });
         },
-        mounted() {}
-
-    }
+    },
+    computed: {
+        username: function() {
+            return this.$store.state.username;
+        }
+    },
+    mounted() {}
+};
 </script>
